1a8fc3903ffb5eb4f6248d1df280f54e25b1682d
Adolfo Cruz <adolfo@rewrite.com.br>
2022-06-02 21:44:23 -0400

    main - aula #03

--- src/app/meu-primeiro/meu-primeiro.component.ts
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'meu-primeiro-component',
   template: `
-    <p>Meu primeiro component com Angular!</p>
+    <p>Meu primeiro component (manual) com Angular!</p>
   `
 })
 export class MeuPrimeiroComponent { }
