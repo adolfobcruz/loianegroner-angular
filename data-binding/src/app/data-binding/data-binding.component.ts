d2510b7cbbfeccf98e5263dce1b6076eecaf3791
Adolfo Cruz <adolfo@rewrite.com.br>
2022-08-17 16:50:10 -0400

    main - aulas #09

---
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+  url:          string  = 'http://rewrite.com.br';
+  cursoAngular: boolean = true;
+  urlImagem:    string  = 'https://picsum.photos/400/200/';
+
+  getValor() {
+    return 1;
+  }
+
+  getCurtirCurso() {
+    return true;
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
